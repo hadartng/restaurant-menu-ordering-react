933d8b01cbbee668bc81f7eddc0d48ce029e9f29
hadartng <128438624+hadartng@users.noreply.github.com>
2023-03-25 06:49:06 +0300

    Small touch for the Reviews component (just a placeholder for now)

--- src/components/Reviews/ReviewsCarousel.tsx
+++ src/components/Reviews/ReviewsCarousel.tsx
@@ -1,5 +1,5 @@
 import styles from "./ReviewsCarousel.module.css";
-import reviews from './reviewsItems';
+import reviews from './utils/reviewsItems';
 
 function ReviewsCarousel() {
     return (
---
+++ src/components/Reviews/utils/reviewsItems.ts
@@ -0,0 +1,69 @@
+import {Review} from '../../../features/types';
+
+// For testing purpose
+const reviewsItems: Review[] = [
+    {
+        id: 'rec1',
+        name: 'הדר',
+        date: "05/02/2023 09:24",
+        score: 10,
+        review: 'טעייייייים',
+    },
+    {
+        id: 'rec2',
+        date: "07/03/2023 13:55",
+        name: 'בן',
+        score: 8,
+        review: 'אספר לכל חבריי! פשוט מעולה! רק אם אפשר להוסיף את הבירה בעסקית בבקשה',
+    },
+    {
+        id: 'rec3',
+        name: 'אנונימי',
+        date: "23/03/2023 13:00",
+        score: 1,
+        review: 'לא אהבתי',
+    },
+    {
+        id: 'rec4',
+        name: 'תומר',
+        date: "05/02/2023 09:24",
+        score: 9,
+        review: 'אף פעם לא מאכזבים!',
+    },
+    {
+        id: 'rec5',
+        date: "07/03/2023 13:55",
+        score: 8,
+        name: 'אורית',
+        review: 'מתה על הספיישלים!',
+    },
+    {
+        id: 'rec6',
+        name: 'אנונימית',
+        date: "23/03/2023 13:00",
+        score: 2,
+        review: 'חבל שהכל תמיד מגיע קר...',
+    },
+    {
+        id: 'rec7',
+        name: 'עידו',
+        date: "05/02/2023 09:24",
+        score: 10,
+        review: 'אין שבוע בלי מרלן!!!',
+    },
+    {
+        id: 'rec8',
+        date: "07/03/2023 13:55",
+        score: 9,
+        name: 'נעמה',
+        review: 'אין תלונות',
+    },
+    {
+        id: 'rec9',
+        name: 'שחר',
+        date: "23/03/2023 13:00",
+        score: 1,
+        review: 'כנראה הגעתי ביום גרוע כי ממש לא הבנתי על מה כל ההייפ',
+    },
+];
+export default reviewsItems;
