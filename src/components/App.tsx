bd60f34996e6e2c6f6338516913ea3dab1fb7b2f
hadartng <hadar223@gmail.com>
2023-04-01 06:03:45 +0300

    Temporarily hide Reviews comp.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,7 +12,7 @@ function App() {
             <div className={styles.app}>
                 <Header/>
                 <Menu/>
-                <Reviews/>
+                {/*<Reviews/>*/}
             </div>
         </Provider>
     );
